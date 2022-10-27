7a4ce452278c2b1f296d1c5e5b33756a75f2f82e
Dagm Tadele Kebede <78392962+dkebede202@users.noreply.github.com>
2022-10-26 21:02:27 -0400

    log-in and registeration page are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './components/pages/LandingPage'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
+import HomePage from './components/pages/HomePage'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and do whatever you want.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route exact path="/" element={ <LandingPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/register" element={ <RegisterPage/> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+                    <Route path="/home" element={ <HomePage/> } />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">IZEMSPOT</a></p>
+    )
+}
+
+const FooterStyle = {
+    background: "#222",
+    fontSize: ".8rem",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+    opacity: ".5"
+}
