590ae59a857f3539bea339614442b608e03688a5
Graeme <southworthgraeme@gmail.com>
2022-12-01 15:09:57 -0500

    Added changes to reformat date list

--- client/src/components/pages/ApexChart.js
+++ client/src/components/pages/ApexChart.js
@@ -66,7 +66,12 @@ const TableCoin=()=> {
   }
 
 async function setChart(){
-  const b = await fetch('http://localhost:5000/graphdata/2022-01-01/2022-01-02/C8840AED-9AC5-4F51-B1F3-8212FC3F5F0A/false').then(res => res.json());
+  //I want the 3 inputs (start time, end time, and api key) to be variables to 
+  const hraphData = await fetch('http://localhost:5000/graphdata/2022-01-01/2022-01-02/C8840AED-9AC5-4F51-B1F3-8212FC3F5F0A/false').then(res => res.json());
+  console.log(b);
+  for (let i = 0; i < graphData["data"].length; i++) {
+    graphData["data"][i]["x"] =  new Data(graphData["data"][i]["x"])
+  }
 }
 
 class ApexChart extends React.Component {
